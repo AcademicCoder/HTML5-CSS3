75c3c35c4d29f25b5ca3be14e54f2c0364297d3a
mohamedabdiahmed <mohamedahmedabdiah@gmail.com>
2022-09-11 18:14:25 +0300

    waxaan kusoo daray casharo dheer ah

---
+++ sharaxaad/form.s
@@ -0,0 +1,39 @@
+ <!-- form waa halka uu qofku xogtiisa kusoo gelinayo si loo aqoonsado  -->
+
+    <!-- 
+        action : waa atribute shaqadiisu tahay in aad formka kusoo xidhi kartid qaybta logic ga tusaale luuqada aad u isticmaalayso sida php javascript java ...
+        method: waxa ay u sheegeysa qofka nooca xogta ma mid lasoo qadanaya mise waa mid la dirayo waa mid la dirayo 
+        waxana loo yaqaan request method waana afar nooc :
+        1. Post : oo ah diristii 
+        2. Get : oo helistii ah 
+        3. delete : oo delete garayntii ah 
+        4. patch: waa mid kuu saamaxaya in xogtii ka dhigi kara mid qayb ahaan la badali karo.
+
+     -->
+
+<!-- 
+
+maxaa la iga doonaya in aan qofkii form ahaan uga diiwaangeliyo 
+
+1. magaca 
+2. email 
+3. password
+4.jinsiga
+5. wadanka ama gobolka uu joogo 
+6. telephankiisa
+
+-->
+
+<!-- 
+hadaba maxaa u baahanay 
+
+1. meshi magaca lagu qorayay 
+2. meshi emailka 
+3. messhi passwordka
+4. meshi aan jinsiga kuso bandhigayay rag ama dumar 
+5. meshi wadamada
+6. meshi telephone ka nooca loo adeegsanayo 
+7. halkii laga dirayay 
+8. halkii laga masaxayay
+
+-->
