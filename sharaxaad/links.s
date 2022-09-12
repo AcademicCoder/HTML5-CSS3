b3fbafd6ab3dff38e1931fd63a0f0a6cd4bcbe53
mohamedabdiahmed <mohamedahmedabdiah@gmail.com>
2022-09-12 16:42:18 +0300

    waxaan kusoo daray casharo dheeri ah css

---
+++ sharaxaad/links.s
@@ -0,0 +1,16 @@
+ <!-- html link waxay inaga caawinaysa in aad website kaaga kusoo xidhi karto websites sida social media website => tusaale facebook google whatsappweb -->
+    <!-- waxa jira laba nooc oo linkga loo sameeyo 
+    1. inaad websites ku xidhi karto webkaga
+    2. inad kusoo xidhi karto pages ka kale ee website kagu ka kooban yahay hore waxaan usoo sheegnay in website kaagu ka kooban yahay webpages isku tagay
+    -->
+
+    <!-- habka website ahaan loogu xidhi karo  -->
+    <!-- a : waa tagga shaqadan inoo qabanaya 
+         href(hypertext reference ) oo ah halka uu ka imanayo linkgu ama uu ku socdo 
+         target: waxay inoo fududaynaysa webpage kan qaabka lagu soo furayo 3hab ayana ugu caansan gudihiisa 
+        1. _blank: oo ah inaad page kale kusoo kiciso iyadoo kan asalka ahi joogayo 
+        2. _self : oo ah mid isla page kan asalka ah lagu soo dhex furayo 
+        3._parent : oo ah mid kusoo dhex furmaya isla ka uu joogo
+        -->
+
+    
